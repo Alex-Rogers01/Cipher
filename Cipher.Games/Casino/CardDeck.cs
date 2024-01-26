8af2114720d7e161263aa54a3d507a8b93eb9ddc
Alex Rogers <arogers@datumtechnology.com>
2024-01-26 11:18:47 +0000

    Added card deck drawing and added tests

--- Cipher.Games/Casino/CardDeck.cs
+++ Cipher.Games/Casino/CardDeck.cs
@@ -4,11 +4,14 @@ namespace Cipher.Games.Casino;
 public class CardDeck
 {
   public List<Card> Cards { get; private set; } = new List<Card>();
+  private int DecksUsed = 1;
 
   public CardDeck(int nNumberOfDecksUsed = 1)
   { 
     if(nNumberOfDecksUsed < 1)
       nNumberOfDecksUsed = 1;
+
+    this.DecksUsed = nNumberOfDecksUsed;
 
     for(int x = 0; x < nNumberOfDecksUsed; x++)
     { 
@@ -16,7 +19,7 @@ public class CardDeck
       { 
         for(int z = 0; z < 13; z++)
         { 
-          Cards.Add(new Card((CardSuit)y + 1, z+1));
+          this.Cards.Add(new Card((CardSuit)y + 1, z+1));
         }
       }
     }
@@ -24,5 +27,25 @@ public class CardDeck
 
   public CardDeck() : this(1)
   {  
+  }
+
+  public Card DrawCard()
+  { 
+    Random r = new Random();
+    int index = r.Next(0, this.Cards.Count);
+
+    Card c = this.Cards[index];
+    this.Cards.RemoveAt(index);
+
+    return c;
+  }
+
+  public void Reset(int nNewDecksAddedCount = 0)
+  { 
+    if(nNewDecksAddedCount > 0)
+      DecksUsed = nNewDecksAddedCount;
+
+    CardDeck newDeck = new CardDeck(DecksUsed);
+    this.Cards = newDeck.Cards;
   }
 }
--- Cipher.Tests/Games/Casino/CardDeckTests.cs
+++ Cipher.Tests/Games/Casino/CardDeckTests.cs
@@ -30,4 +30,50 @@ public class CardDeckTests : TestBase
     int expectedCardCount = isDeckUsedValid ? CardNumberCount * nDecksUsed : CardNumberCount;
     Assert.AreEqual(expectedCardCount, Deck.Cards.Count);
   }
+
+
+  [TestMethod]
+  public void TestCardDraw()
+  { 
+    Deck = new CardDeck(1);
+
+    int cardCount = Deck.Cards.Count;
+    Card c = Deck.DrawCard();
+
+    int cardCountNew = Deck.Cards.Count;
+    bool cardExists = Deck.Cards.Where(x => x.CardNumber == c.CardNumber && x.Suit == c.Suit).Count() > 0;
+
+    Assert.IsTrue((cardCount == cardCountNew + 1) && !cardExists);
+  }
+
+  [TestMethod]
+  [DataRow(1)]
+  [DataRow(2)]
+  [DataRow(5)]
+  [DataRow(10)]
+  [DataRow(0)]
+  [DataRow(-1)]
+  public void TestCardDeckReset(int nNumberOfDecks)
+  { 
+    CardDeck _deck = new CardDeck(nNumberOfDecks);
+    
+    if(nNumberOfDecks < 1)
+      nNumberOfDecks = 1;
+
+    Random r = new Random();
+    int _x = r.Next(1, _deck.Cards.Count - 3);
+    int startingDeckCardCount = _deck.Cards.Count;
+
+    for (int x = 0; x < _x; x++)
+      _deck.DrawCard();
+
+    int deckCountAfterDraws = _deck.Cards.Count;
+    _deck.Reset();
+
+    int deckCountAfterReset = _deck.Cards.Count;
+
+    Assert.IsTrue((startingDeckCardCount == nNumberOfDecks * CardNumberCount) &&
+                  (deckCountAfterDraws < startingDeckCardCount) &&
+                  (startingDeckCardCount == deckCountAfterReset));
+  }
 }
